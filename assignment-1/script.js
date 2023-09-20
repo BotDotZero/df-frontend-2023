b69fca20ea37d1508160366bd50544d4a09bcfa3
Trình Thanh Phong <dinhmenhhathui@gmail.com>
2023-09-20 18:40:09 +0700

    done

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -9,7 +9,8 @@ var books = [
   { id: 3, name: "The Phoenix Project", author: "Gene Kim", topic: "DevOps" },
 ];
 
-// Auto function
+/* *********** AUTO FUNCTION *********** */
+
 var loadBooks = () => {
   var booksData = JSON.parse(localStorage.getItem("books"));
   if (booksData == null) {
@@ -21,30 +22,34 @@ var loadBooks = () => {
 
 var tableContent = document.getElementById("booksTableBody");
 
-var fillTable = () => {
+var fillTable = (data) => {
   tableContent.innerHTML = "";
-  books.forEach((book) => {
-    var row = document.createElement("tr");
-    row.setAttribute("id", "book" + book.id);
-    var name = document.createElement("td");
-    name.innerHTML = book.name;
-    var author = document.createElement("td");
-    author.innerHTML = book.author;
-    var topic = document.createElement("td");
-    topic.innerHTML = book.topic;
-    var action = document.createElement("td");
-    var btn = document.createElement("button");
-    btn.innerHTML = "Delete";
-    btn.setAttribute("class", "btn btn-danger");
-    btn.setAttribute("onclick", "delBook(" + book.id + ")");
-    action.appendChild(btn);
+  if (data == null || data.length == 0) {
+    tableContent.innerHTML = "<tr><td style='text-align: center' colspan='4'>No data found</td></tr>";
+  } else {
+    data.forEach((book) => {
+      var row = document.createElement("tr");
+      row.setAttribute("id", "book" + book.id);
+      var name = document.createElement("td");
+      name.innerHTML = book.name;
+      var author = document.createElement("td");
+      author.innerHTML = book.author;
+      var topic = document.createElement("td");
+      topic.innerHTML = book.topic;
+      var action = document.createElement("td");
+      var btn = document.createElement("button");
+      btn.innerHTML = "Delete";
+      btn.setAttribute("class", "btn btn-danger");
+      btn.setAttribute("onclick", "openDelModal(" + book.id + ", '" + book.name + "')");
+      action.appendChild(btn);
 
-    row.appendChild(name);
-    row.appendChild(author);
-    row.appendChild(topic);
-    row.appendChild(action);
-    tableContent.appendChild(row);
-  });
+      row.appendChild(name);
+      row.appendChild(author);
+      row.appendChild(topic);
+      row.appendChild(action);
+      tableContent.appendChild(row);
+    });
+  }
 }
 
 var addBook = (newBook) => {
@@ -52,32 +57,88 @@ var addBook = (newBook) => {
   newBook.id = lastId + 1;
   books.push(newBook);
   localStorage.setItem("books", JSON.stringify(books));
-  fillTable();
+  fillTable(books);
+  addBookModal.style.display = "none";
 }
 
 var delBook = (id) => {
-  if (confirm("Are you sure to delete this book?")) {
+  loadBooks();
+  books = books.filter((book) => book.id != id);
+  localStorage.setItem("books", JSON.stringify(books));
+  fillTable(books);
 
-    loadBooks();
-    books = books.filter((book) => book.id != id);
-    localStorage.setItem("books", JSON.stringify(books));
-    fillTable();
-  }
 };
+// Search function
+var search = document.getElementById("searchArea");
+search.addEventListener("keyup", () => {
+  var searchValue = search.value.toLowerCase();
+  var searchResult = books.filter((book) => {
+    return (
+      book.name.toLowerCase().includes(searchValue) ||
+      book.author.toLowerCase().includes(searchValue) ||
+      book.topic.toLowerCase().includes(searchValue)
+    );
+  });
+  fillTable(searchResult);
+});
 
-// Action function
+/* *********** ACTION FUNCTION *********** */
 
 var createBookBtn = document.getElementById("createBookBtn");
 var newBookFrm = document.getElementById("newBookFrm");
 
-createBookBtn.addEventListener("click", () => {
-  !newBookFrm.checkValidity()
-    ? alert("Please fill all fields!")
-    : addBook({ 'name': document.getElementById('newBookFrm__name').value, 'author': document.getElementById('newBookFrm__author').value, 'topic': document.getElementById('newBookFrm__topic').value }), newBookFrm.reset();
+newBookFrm.addEventListener("submit", () => {
+  addBook({
+    'name': document.getElementById('newBookFrm__name').value,
+    'author': document.getElementById('newBookFrm__author').value,
+    'topic': document.getElementById('newBookFrm__topic').value
+  })
+  newBookFrm.reset();
 });
 
 /* *********** START FUNCTION *********** */
 
 loadBooks();
-fillTable();
+fillTable(books);
 
+/* ************************************** */
+
+
+/* *********** MODAL *********** */
+var addBookModal = document.getElementById("addBookModal");
+var delCfmModal = document.getElementById("delCfmModal");
+
+var addBtn = document.getElementById("addBtn");
+var delBtns = document.getElementsByClassName("delBtn");
+
+addBtn.onclick = function () {
+  addBookModal.style.display = "block";
+}
+
+var cfmBtn = document.getElementById("delCfmBtn");
+cfmBtn.onclick = function () {
+  delBook(this.getAttribute('data-id'));
+  delCfmModal.style.display = "none";
+}
+var openDelModal = (id, name) => {
+  delCfmModal.style.display = "block";
+  document.getElementById('delBookName').innerHTML = name;
+  cfmBtn.setAttribute('data-id', id);
+}
+
+
+var closeBtn = document.getElementsByClassName("modal__cancel");
+// Click [x] or Cancel to close modal
+for (let i = 0; i < closeBtn.length; i++) {
+  closeBtn[i].onclick = function () {
+    addBookModal.style.display = "none";
+    delCfmModal.style.display = "none";
+  }
+}
+// Click outside modal to close
+window.onclick = function (event) {
+  if (event.target == addBookModal || event.target == delCfmModal) {
+    addBookModal.style.display = "none";
+    delCfmModal.style.display = "none";
+  }
+}
